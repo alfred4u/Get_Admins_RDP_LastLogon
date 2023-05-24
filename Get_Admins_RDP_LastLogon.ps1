2f2e5c1522c7094c1f31582c77656b8ba77102a1
Alfred Iskhakov <alfred4u@gmail.com>
2023-05-25 00:57:37 +0300

    init commit

---
+++ Get_Admins_RDP_LastLogon.ps1
@@ -0,0 +1,54 @@
+# Last logon EventID
+$eventID = 5379
+$hostname = hostname
+$GroupAdm = 'Administrators'
+$GroupRDS = 'Remote Desktop Users'
+$Path = '.\'
+
+$AdmMembers = Get-LocalGroupMember -Group $GroupAdm
+$RDSMembers = Get-LocalGroupMember -Group $GroupRDS
+
+#Split Domain and user "DOMAIN\username"
+$admins = @()
+foreach($a in $AdmMembers){
+    $a = ($a -Split '\\')[1]
+    $admObj = [PSCustomObject]@{
+        "Username" = $a
+        "Group" = $GroupAdm
+    }
+    $admins += $admObj
+}
+
+#Split Domain and user "DOMAIN\username" and get username
+$RDS = @()
+foreach($r in $RDSMembers){
+    $r = ($r -Split '\\')[1]
+    
+    $rdsObj = [PSCustomObject]@{
+        "Username" = $r
+        "Group" = $GroupRDS
+    }
+$RDS += $rdsObj
+}
+$allusers = $admins + $rds | Group-Object username
+
+$Result = @()
+
+foreach($i in $allusers){
+
+    $events = Get-EventLog -LogName 'Security' -source microsoft-windows-security-auditing -InstanceId $eventID |
+        Where-Object {($_.replacementstrings[1] -eq $i.Name)} |
+        Select-Object `
+            @{Label='UserName';Expression={$_.replacementstrings[1]}},
+            @{Label='Time';Expression={$_.TimeGenerated.ToString('g')}} -First 1
+    
+    $allObj = [PSCustomObject]@{
+        "Hostname" = $hostname
+        "Account" = $i.Name
+        "LastLogon" = $events.Time
+        "Member" = $i.Group.Group -join ','
+    }
+    $Result += $allObj
+}
+
+$Result | Export-csv -Path "$Path\$hostname-report.csv" -NoTypeInformation -Delimiter ";"
